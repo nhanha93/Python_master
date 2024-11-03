703fa6c5077917824c06266f43e820fabfe80ccf
Nathan <nathan.ha.03@gmail.com>
2024-11-03 14:03:38 -0600

    commit

--- data_processing.py
+++ data_processing.py
@@ -1,18 +1,18 @@
-# sent = input("Enter a sentence: ")
-# print(sent.upper())
-# num_of_words = input("Enter a paragraph: ")
-# print(len(num_of_words))
-# digit_check = input("Enter a string: ")
-# print(digit_check.isdigit())
-# a_to_o = input("Enter another string to replace 'a' with 'o': ")
-# print(a_to_o.replace("a","o"))
+sent = input("Enter a sentence: ")
+print(sent.upper())
+num_of_words = input("Enter a paragraph: ")
+print(len(num_of_words))
+digit_check = input("Enter a string: ")
+print(digit_check.isdigit())
+a_to_o = input("Enter another string to replace 'a' with 'o': ")
+print(a_to_o.replace("a","o"))
 def initials(name):
     name_parts = name.split()
     initials = [part[0].upper() for part in name_parts]
     return initials
 
 full_name = input("Enter first and last name: ")
-print("Your initials are: ", initials(full_name))
+print("Your initials are: ", *initials(full_name))
 
-    
-
+input_string = input("Enter yet another string: ")
+print("The length of your string is: ", len(input_string))
